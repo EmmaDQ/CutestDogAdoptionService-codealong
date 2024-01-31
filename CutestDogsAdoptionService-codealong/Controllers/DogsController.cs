a98cd6807ccd9b7c223f66cf0a0182fce18cf871
EmmaDQ <sydemma@hotmail.com>
2024-01-31 11:40:56 +0100

    add views

--- CutestDogsAdoptionService-codealong/Controllers/DogsController.cs
+++ CutestDogsAdoptionService-codealong/Controllers/DogsController.cs
@@ -5,7 +5,7 @@ namespace CutestDogsAdoptionService_codealong.Controllers
 {
     public class DogsController : Controller
     {
-        public List<DogModel> dogs = new List<DogModel>
+        public static List<DogModel> dogs = new List<DogModel>
 {
     new DogModel{ Id = 1, Name = "Bark Twain", Cuteness = 9, Image = "doggo 1.png", FavFood = "Barkoni", FavToy = "Barkle", Temperament = 7, IsAdopted= false },
     new DogModel{ Id = 2, Name = "Sir Waggington", Cuteness = 8, Image = "doggo 2.png", FavFood = "Pawsta", FavToy = "Fetch Stick", Temperament = 6, IsAdopted= false },
@@ -27,6 +27,26 @@ namespace CutestDogsAdoptionService_codealong.Controllers
             List<DogModel> sortedDogs = dogs.OrderBy(d => d.Name).ToList();
 
             return View(sortedDogs);
+        }
+
+        public IActionResult Details(int id)
+        {
+            //Hämta hunden med id:t
+            DogModel? selectedDog = dogs.FirstOrDefault(d => d.Id == id);
+
+            return View(selectedDog);
+        }
+
+        public IActionResult Adopt(int id)
+        {
+            DogModel? selectedDog = dogs.FirstOrDefault(d => d.Id == id);
+
+            if (selectedDog != null)
+            {
+                selectedDog.IsAdopted = true;
+            }
+
+            return View(selectedDog);
         }
     }
 }
--- CutestDogsAdoptionService-codealong/Program.cs
+++ CutestDogsAdoptionService-codealong/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dogs}/{action=Index}/{id?}");
 
 app.Run();
