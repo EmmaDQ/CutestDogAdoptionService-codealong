42e4588c9d7b0c15c710dccbf34908f0ce81f8ad
EmmaDQ <sydemma@hotmail.com>
2024-01-31 10:04:24 +0100

    Add dogs controller

---
+++ CutestDogsAdoptionService-codealong/Controllers/DogsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CutestDogsAdoptionService_codealong.Controllers
+{
+    public class DogsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
